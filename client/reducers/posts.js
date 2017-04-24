7b4cd7592833314bf482b371aad57ea8ea9432ca
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-24 19:29:05 +0700

    Get the posts reducer working

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,11 +1,19 @@
 const posts = (state = [], action) => {
   switch (action.type) {
     case 'INCREMENT_LIKES': {
-      return state
+      const index = action.index
+      return [
+        ...state.slice(0, index), // before updating
+        {
+          ...state[index],
+          likes: state[index].likes + 1
+        },
+        ...state.slice(index + 1) // after updating
+      ]
       break
     }
+    default: return state
   }
-  return state
 }
 
 export default posts
