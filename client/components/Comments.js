85feb5e03e0a6dbdc4b9e99aa450d3b6cbd4beb3
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-24 20:48:55 +0700

    Get the Comments work with its reducer

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,50 +1,43 @@
 import React from 'react'
 
-class Comments extends React.Component {
-  renderComment (comment, i) {
+const Comments = React.createClass({
+  renderComment (comment, index) {
     return (
-      <div className='comment' key={i}>
+      <div className='comment' key={index}>
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className='remove-comment' onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
+          <button className='remove-comment' onClick={this.props.removeComment.bind(null, this.props.params.postId, index)}>&times;</button>
         </p>
       </div>
     )
-  }
-
-  renderCommentForm () {
-    return (
-      <form ref='commentForm' className='comment-form' onSubmit={this.handleSubmit.bind(this)}>
-        <input type='text' ref='author' placeholder='author name' />
-        <input type='text' ref='comment' placeholder='comment text' />
-        <input type='submit' hidden />
-      </form>
-    )
-  }
+  },
 
   handleSubmit (e) {
     e.preventDefault()
     this.props.addComment(
       this.props.params.postId,
       this.refs.author.value,
-      this.refs.comment.value
-    )
+      this.refs.comment.value)
     this.refs.commentForm.reset()
-  }
+  },
 
   render () {
     const comments = this.props.comments[this.props.params.postId] || []
-
     return (
       <div className='comments'>
 
         {comments.map(this.renderComment)}
 
-        {this.renderCommentForm()}
+        <form onSubmit={this.handleSubmit} ref='commentForm' className='comment-form'>
+          <input type='text' ref='author' placeholder='author' />
+          <input type='text' ref='comment' placeholder='comment' />
+          <input type='submit' hidden />
+        </form>
       </div>
     )
   }
-}
+
+})
 
 export default Comments
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,15 +1,33 @@
-const posts = (state = [], action) => {
+const postComments = (state = [], action) => {
   switch (action.type) {
     case 'ADD_COMMENT': {
-      return state
-      break
+      return [
+        ...state, {
+          user: action.author,
+          text: action.comment
+        }
+      ]
     }
     case 'REMOVE_COMMENT': {
-      return state
-      break
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1)
+      ]
+    }
+    default: return state
+  }
+  return state
+}
+
+const comments = (state = [], action) => {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state, // Take the current state
+      // overwrite this post with the new one
+      [action.postId]: postComments(state[action.postId], action)
     }
   }
   return state
 }
 
-export default posts
+export default comments
