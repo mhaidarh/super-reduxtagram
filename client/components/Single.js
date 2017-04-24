e67678ce6c5a0803f9c37134c95ce5290343d062
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-24 19:37:47 +0700

    Get the Single show the Photo

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
+import Photo from './Photo'
+// import Comments from './Comments'
+
 class Single extends Component {
   render () {
+    const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
+    const post = this.props.posts[index]
     return (
       <div className='single-photo'>
-        SINGLE PHOTO
+        <Photo index={index} post={post} {...this.props} />
       </div>
     )
   }
