41a5c630b92353066d4bd47df1a6a3bc183b5bfa
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-24 19:11:01 +0700

    Display Photo in PhotoGrid

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
+import Photo from './Photo'
+
 class PhotoGrid extends Component {
   render () {
     return (
       <div className='photo-grid'>
-        GRID PHOTO
+        {this.props.posts.map((post, index) => {
+          return (
+            <Photo
+              {...this.props}
+              key={post.id}
+              index={index}
+              post={post}
+            />
+          )
+        })}
       </div>
     )
   }
